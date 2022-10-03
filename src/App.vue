cf90eeb4eb437a6b93fc804476c61a7409ef1975
kalaw410 <87921899+kalaw410@users.noreply.github.com>
2022-10-03 13:57:57 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <router-link to="/about"><img src="../akipop.jpg" width="96" alt="brand-logo"> &emsp; &emsp; &emsp;&emsp; &emsp; &emsp;&emsp; &emsp; &emsp;</router-link> 
+    <router-link to="/home">Home &emsp; &emsp; &emsp; &emsp;</router-link>    
+    <router-link to="/about">About &emsp; &emsp; &emsp; &emsp;</router-link>    
+    <router-link to="/cart">Cart &emsp; &emsp; &emsp; &emsp;</router-link>    
+    <router-link to="/product">Product &emsp; &emsp; &emsp; &emsp;</router-link> 
+    <router-link to="/register">Register &emsp; &emsp; &emsp; &emsp;</router-link>
+    <router-link to="/login">Log In &emsp; &emsp; &emsp; &emsp;</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin: -10px -10px;
+}
+nav {
+  padding: 20px;
+  background-color: #e95a85;
+  margin: auto;
+  max-width: 100%;
+}
+nav a {
+  font-weight: bold;
+  color: #ffffff;
+  text-decoration: none;
+}
+nav a.router-link-exact-active {
+  color: #f7c7c7;
+}
+</style>
