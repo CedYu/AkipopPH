80a50c0318426fa1a8bc36bc773cc918666b59f3
Ced <79917173+Revkxd@users.noreply.github.com>
2022-10-21 11:39:01 +0800

    Create functions.js

---
+++ Node/functions.js
@@ -0,0 +1,14 @@
+function Register(){
+    // Check if user exists
+    // Add user to database
+}
+
+function Login(){
+    // Check if user exists
+    // Check if password is correct
+    // Create session key
+}
+
+function Logout(){
+    // Delete session key
+}
