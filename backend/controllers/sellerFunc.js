9f46489802827f8f5eacc92f3614c905e62a96d6
Ced <79917173+Revkxd@users.noreply.github.com>
2022-10-25 22:36:28 +0800

    Created sellerFunc.js file

---
+++ backend/controllers/sellerFunc.js
@@ -0,0 +1,18 @@
+const sellerFunc = {
+    AddItem: function(req, res){
+
+    },
+
+    EditItem: function(req, res){
+
+    },
+
+    DeleteItem: function(req, res){
+
+    },
+
+    CreateVoucher: function(req, res){},
+    DeleteVoucher: function(req, res){}
+}
+
+export default sellerFunc
