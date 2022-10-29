7bee138c1a292c2b2ef784c31acc5edc51b3c1af
DDGC7 <99391788+DDGC7@users.noreply.github.com>
2022-10-29 21:29:01 +0800

    Added category in product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose'
 const productSchema = new mongoose.Schema({
   image: {},
   productID: { type: String, required: true, unique: true },
-	name: { type: String, required: true },
+  name: { type: String, required: true },
+  category: {type: String, requireed: true},
   description: { type: String, required: true },
   price: { type: Number, required: true },
-	stock: { type: Number, required: true },
+  stock: { type: Number, required: true },
 })
 
 const Product = mongoose.model("Product", productSchema)
