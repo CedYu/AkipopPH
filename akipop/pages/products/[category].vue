a0496d90fe235b763c8498829bdca1ed3453e660
Ced <79917173+Revkxd@users.noreply.github.com>
2022-11-16 21:20:12 +0800

    im bobofied

---
+++ akipop/pages/products/[category].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Category: {{category}}</h1>
+        <p>So ayown yung category nato and2 bahaha</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ akipop/pages/products/plushies.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Yes plushies</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
