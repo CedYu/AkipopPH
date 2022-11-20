d1917ba07ee53e826d2abed4e9d477aa26fa235f
DDGC7 <duannecruz7@gmail.com>
2022-11-20 14:34:14 +0800

    Update index.vue

--- akipop/pages/products/index.vue
+++ akipop/pages/products/index.vue
@@ -2,45 +2,37 @@
   <br><br><br><br><br><br><br><br><br><br>
   <h1>Products</h1>
   <!--WITH SCRIPT-->
-  <div v-for="mainProduct in mainProducts"
-      :key="mainProduct.id" 
+  <div v-for="product in products"
+      :key="product.id" 
       class="mainProduct">
   
     <img src="../../assets/img/product.jpg" alt="Denim Jeans" style="width:100%">
   
-    <p>{{mainProduct.name}}</p>
-    <p class="price">{{mainProduct.price}}</p>
-    <p>{{mainProduct.description}}</p>
-    <p>{{mainProduct.price}}</p>
-    <p>{{mainProduct.quantity}}</p>
+    <p>{{product.name}}</p>
+    <p class="price">{{product.price}}</p>
+    <p>{{product.description}}</p>
+    <p>{{product.price}}</p>
+    <p>{{product.quantity}}</p>
     <p><button>Add to Cart</button></p>
     <p><button><router-link to="/products">See More Details</router-link></button></p>
   </div>
   </template>
 
-<script> 
+<script>
 export default{
+  async created() {
+    const supabase = useSupabaseClient()
+    const { data , error } = await supabase
+      .from('products')
+      .select()
+    this.products = data
+    console.log(data)
+  },
   data(){
       return{
-          mainProducts:[
-          {
-              id:"19708555531",
-              name: "SPY X FAMILY PILLOW PLUSHIE Anya",
-              description: "High quality pillow with Anya design",
-              price: '$13.66',
-              quantity: "15",
-              category: "plushies"
-          },
-          {
-              id:"18308562708",
-              name: "SPY X FAMILY PILLOW prints Pink",
-              description: "High quality pillow with elegant design",
-              price: '$45.66',
-              quantity: "125",
-              category: "prints"
-          },
-      ],
-  }
+          products:[],
+      }
+  },
 }
-}
+
 </script>
