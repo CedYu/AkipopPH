55801d4e735981fd0f6df94946721789136ca7ba
Ultix1 <wai_kei_li@dlsu.edu.ph>
2022-11-20 22:04:57 +0800

    Update auth.ts

--- akipop/middleware/auth.ts
+++ akipop/middleware/auth.ts
@@ -1,7 +1,5 @@
 export default defineNuxtRouteMiddleware((to) => {
   const user = useSupabaseUser()
-
-  console.log(user.value)
   if (user.value && to.path==='/account/login'){
     return navigateTo('/')
   }
