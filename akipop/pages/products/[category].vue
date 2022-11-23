173f989a25cd415f2401b7c48f84c395fdc16034
DDGC7 <duannecruz7@gmail.com>
2022-11-24 04:47:22 +0800

    Update [category].vue

--- akipop/pages/products/[category].vue
+++ akipop/pages/products/[category].vue
@@ -1,6 +1,17 @@
 <template>
  <br><br><br><br><br><br><br><br><br><br>
  <h1>{{category}}</h1>
+ <div class="filter-container">
+  <select id="list" v-on:change="getAnimeName()">
+    <option value=''>-- Select Anime --</option>
+    <option value='Spy X Family'>Spy X Family</option>
+    <option value='Naruto'>Naruto</option>
+    <option value='Kuroko no Basket'>Kuroko no Basket</option>
+    <option value='Genshin Impact'>Genshin Impact</option>
+    <option value='Chainsaw Man'>Chainsaw Man</option>
+    <!-- <option v-for="anime in animes" v-bind:key="anime.id" value =${{anime.name}}>{{anime.name}}</option > -->
+  </select>
+ </div>
  <div class = "container">
     <productcard v-for="product in products" v-bind:key="product.id" :product="product"/>
   </div>    
@@ -17,18 +28,39 @@ export default{
       .eq('category',this.category)
       .order('id', { ascending: true })
     this.products = data
+
+    const { data:animes} = await supabase
+      .from('animes')
+      .select()
+    this.animes = animes
     },
     data(){
-        
         return{
             products:[],
+            animes:[],
+            anime:[],
             category:useRoute().params.category
         }
+    },
+    methods:{
+      async getAnimeName(){
+          this.anime = document.getElementById("list").value;
+          const supabase = useSupabaseClient()
+          const { data , error } = await supabase
+            .from('products')
+            .select()
+            .eq('category',this.category)
+            .eq('anime',this.anime)
+            .order('id', { ascending: true })
+          this.products = data
+      }
     }
+
 }
 </script>
 
 <style scoped>
+
   .container{
     display: flex;
     flex-wrap: wrap;
