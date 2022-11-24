d10ab5e6faf75222b37c458f34e9724ed6124bc7
Ced <79917173+Revkxd@users.noreply.github.com>
2022-11-24 14:39:25 +0800

    Added anime filter to all products

--- akipop/pages/products/[category].vue
+++ akipop/pages/products/[category].vue
@@ -1,72 +1,69 @@
 <template>
- <br><br><br><br><br><br><br><br><br><br>
- <h1 class="category-header">{{category}}</h1>
- <div class="filter-container">
+  <br><br><br><br><br><br><br><br><br><br>
+  <h1 class="category-header">{{category}}</h1>
+  <div class="filter-container">
   <select id="list" v-on:change="getAnimeName()">
     <option id="option-font">-- Select Anime --</option>
-    <option id="option-font" v-for="anime in animes" v-bind:key="anime.id">{{anime.name}}</option >
+    <option id="option-font" v-for="anime in animes" v-bind:key="anime.id">{{anime.name}}</option>
   </select>
- </div><br>
- <div class = "container">
+  </div>
+  <div class = "container">
     <productcard v-for="product in products" v-bind:key="product.id" :product="product"/>
   </div>    
-
 </template>
 
 <script>
 export default{
     async created() {
-    const supabase = useSupabaseClient()
-    const { data , error } = await supabase
-      .from('products')
-      .select()
-      .eq('category',this.category)
-      .order('id', { ascending: true })
-    this.products = data
+      const supabase = useSupabaseClient()
+      const { data , error } = await supabase
+        .from('products')
+        .select()
+        .eq('category',this.category)
+        .order('id', { ascending: true })
+      this.products = data
 
-    const { data:animes} = await supabase
-      .from('animes')
-      .select()
-    this.animes = animes
+      const { data:animes} = await supabase
+        .from('animes')
+        .select()
+      this.animes = animes
     },
     data(){
         return{
-            products:[],
-            animes:[],
-            anime:[],
-            category:useRoute().params.category
+          products:[],
+          animes:[],
+          anime:[],
+          category:useRoute().params.category
         }
     },
     methods:{
       async getAnimeName(){
-          var e = document.getElementById("list");
-          this.anime = e.options[e.selectedIndex].text
-          const supabase = useSupabaseClient()
-          if(this.anime=='-- Select Anime --'){
-            const { data , error } = await supabase
-              .from('products')
-              .select()
-              .eq('category',this.category)
-              .order('id', { ascending: true })
-            this.products = data
-          }
-          else{
-            const { data , error } = await supabase
+        var e = document.getElementById("list");
+        this.anime = e.options[e.selectedIndex].text
+        const supabase = useSupabaseClient()
+        if(this.anime=='-- Select Anime --'){
+          const { data , error } = await supabase
             .from('products')
             .select()
             .eq('category',this.category)
             .order('id', { ascending: true })
-            .eq('anime',this.anime)
           this.products = data
-          }
+        }
+        else{
+          const { data , error } = await supabase
+          .from('products')
+          .select()
+          .eq('category',this.category)
+          .order('id', { ascending: true })
+          .eq('anime',this.anime)
+        this.products = data
+        }
       }
     }
-
 }
 </script>
 
 <style scoped>
-
   .container{
     display: flex;
     flex-wrap: wrap;
--- akipop/pages/products/index.vue
+++ akipop/pages/products/index.vue
@@ -1,6 +1,12 @@
 <template>
   <br><br><br><br><br><br><br><br><br><br>
   <h1>Products</h1>
+  <div class="filter-container">
+  <select id="list" v-on:change="getAnimeName()">
+    <option id="option-font">-- Select Anime --</option>
+    <option id="option-font" v-for="anime in animes" v-bind:key="anime.id">{{anime.name}}</option>
+  </select>
+  </div>
   <div class="container">
     <productcard v-for="product in products" v-bind:key="product.id" :product="product"/>
   </div>
@@ -24,6 +30,14 @@
 		animation: fadeInDown;
 		animation-duration: 1s;
   }
+
+  #list{
+    border-radius: 10px;
+  }
+
+  #option-font{
+    font-family: lemonmilkreg;
+  }
 </style>
 
 <script>
@@ -35,12 +49,40 @@ export default{
       .select()
       .order('id', { ascending: true })
     this.products = data
-    console.log(data)
+    
+    const { data:animes} = await supabase
+      .from('animes')
+      .select()
+    this.animes = animes
   },
   data(){
     return{
       products:[],
+      animes:[],
+      anime:[]
     }
   },
+  methods:{
+    async getAnimeName(){
+      var e = document.getElementById("list");
+      this.anime = e.options[e.selectedIndex].text
+      const supabase = useSupabaseClient()
+      if(this.anime=='-- Select Anime --'){
+        const { data , error } = await supabase
+          .from('products')
+          .select()
+          .order('id', { ascending: true })
+        this.products = data
+      }
+      else{
+        const { data , error } = await supabase
+        .from('products')
+        .select()
+        .order('id', { ascending: true })
+        .eq('anime',this.anime)
+        this.products = data
+      }
+    }
+  }
 }
 </script>
