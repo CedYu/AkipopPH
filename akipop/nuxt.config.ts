0de584930b7e534a429a6b3303fa170c2d213c0f
Ced <79917173+Revkxd@users.noreply.github.com>
2022-12-09 00:52:19 +0800

    test

--- akipop/nuxt.config.ts
+++ akipop/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+require('dotenv').config()
 export default defineNuxtConfig({
   css: ['animate.css/animate.min.css', '/assets/styles.css'],
   components: true,
