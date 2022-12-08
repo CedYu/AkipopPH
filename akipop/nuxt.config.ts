e92ce4c231e894ea91e000af218b5c7a4f72a50c
Ced <79917173+Revkxd@users.noreply.github.com>
2022-12-09 00:54:30 +0800

    234234234234

--- akipop/nuxt.config.ts
+++ akipop/nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-require('dotenv').config()
 export default defineNuxtConfig({
   css: ['animate.css/animate.min.css', '/assets/styles.css'],
   components: true,
-  modules:['@nuxtjs/supabase'],
+  modules:['@nuxtjs/supabase', '@nuxtjs/dotenv'],
   app:{
     head:{
       charset:'utf-8', viewport:'width=500 initial-scale=1',
